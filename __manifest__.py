24a1e568ed554289c0c3d6077d1927d4d9ba1514
Nguyen Huu Thinh <54807522+thinhnguyen17596@users.noreply.github.com>
2021-04-01 15:05:11 +0700

    Update __manifest__.py
    ch

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,6 @@
         Ứng dụng quản lý việc học từ vựng
     """,
 
-    'author': "Nguyen Ngoc Tu",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
